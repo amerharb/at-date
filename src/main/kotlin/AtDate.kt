75288a1bbf1a9b7d319151df3f4d88ef851b56db
Amer Harb <amer.harb@gmail.com>
2023-04-11 21:14:03 +0200

    add AtDate class

--- src/main/kotlin/AtDate.kt
+++ src/main/kotlin/AtDate.kt
@@ -1,4 +1,12 @@
-fun main(args: Array<String>) {
-    println("@Date")
-    println("Program arguments: ${args.joinToString()}")
-}
+data class AtDate(
+    val rangeLevel: UByte = 0U,
+    val resolutionLevel: UByte = 0U,
+    val zoneLevel: UByte = 0U,
+    val accuracyLevel: UByte = 0U,
+
+    val date: ULong = 0U,
+    val time: ULong = 0U,
+    val zone: ULong = 0U,
+    val plusLeapSeconds: ULong = 0U,
+    val minusLeapSeconds: ULong = 0U,
+)
