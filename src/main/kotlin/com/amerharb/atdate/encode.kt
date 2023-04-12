cb26e723c58d794839731f7c79f79ac762ecf298
Amer Harb <amer.harb@gmail.com>
2023-04-13 00:27:09 +0200

    fix JDN

--- src/main/kotlin/com/amerharb/atdate/encode.kt
+++ src/main/kotlin/com/amerharb/atdate/encode.kt
@@ -26,29 +26,29 @@ fun encode(input: String): AtDate {
     val rValue = propArr.find { it.startsWith("t:") }?.substringAfter(":")
     val r = rValue?.toUInt() ?: 0U
     val resolutionLevel = when (r) {
-            0U -> ResolutionLevel.Level0
-            1U -> ResolutionLevel.Level1
-            2U -> ResolutionLevel.Level2
-            3U -> ResolutionLevel.Level3
-            4U -> ResolutionLevel.Level4
-            5U -> ResolutionLevel.Level5
-            6U -> ResolutionLevel.Level6
-            7U -> ResolutionLevel.Level7
-            8U -> ResolutionLevel.Level8
-            9U -> ResolutionLevel.Level9
-            10U -> ResolutionLevel.Level10
-            11U -> ResolutionLevel.Level11
-            12U -> ResolutionLevel.Level12
-            13U -> ResolutionLevel.Level13
-            14U -> ResolutionLevel.Level14
-            15U -> ResolutionLevel.Level15
-            16U -> ResolutionLevel.Level16
-            17U -> ResolutionLevel.Level17
-            18U -> ResolutionLevel.Level18
-            19U -> ResolutionLevel.Level19
-            20U -> ResolutionLevel.Level20
-            else -> throw Exception("Invalid resolution level")
-        }
+        0U -> ResolutionLevel.Level0
+        1U -> ResolutionLevel.Level1
+        2U -> ResolutionLevel.Level2
+        3U -> ResolutionLevel.Level3
+        4U -> ResolutionLevel.Level4
+        5U -> ResolutionLevel.Level5
+        6U -> ResolutionLevel.Level6
+        7U -> ResolutionLevel.Level7
+        8U -> ResolutionLevel.Level8
+        9U -> ResolutionLevel.Level9
+        10U -> ResolutionLevel.Level10
+        11U -> ResolutionLevel.Level11
+        12U -> ResolutionLevel.Level12
+        13U -> ResolutionLevel.Level13
+        14U -> ResolutionLevel.Level14
+        15U -> ResolutionLevel.Level15
+        16U -> ResolutionLevel.Level16
+        17U -> ResolutionLevel.Level17
+        18U -> ResolutionLevel.Level18
+        19U -> ResolutionLevel.Level19
+        20U -> ResolutionLevel.Level20
+        else -> throw Exception("Invalid resolution level")
+    }
 
     val aValue = propArr.find { it.startsWith("a:") }?.substringAfter(":")
     val accuracy = when (aValue?.lowercase()) {
@@ -63,7 +63,7 @@ fun encode(input: String): AtDate {
     val lList = lValue?.split("-")?.map { it.toULong() } ?: listOf(0UL, 0UL)
     val pl = lList[0]
     val ml = lList[1]
-    val leapSecondsFlag:UByte = when {
+    val leapSecondsFlag: UByte = when {
         (pl == 0UL && ml == 0UL) -> 0U
         (pl < 0xFFUL && ml < 0xFFUL) -> 1U
         (pl < 0xFFFFUL && ml < 0xFFFFUL) -> 2U
@@ -108,6 +108,7 @@ fun encode(input: String): AtDate {
             // only most right 15 bits are used
             jdn.toULong() and 0b01111111_11111111UL
         }
+
         RangeLevel.Level2 -> TODO()
         RangeLevel.Level3 -> TODO()
         RangeLevel.Level4 -> TODO()
@@ -138,6 +139,7 @@ fun encode(input: String): AtDate {
             // count nanoseconds
             hour * 3600_000_000_000UL + minute * 60_000_000_000UL + (second * 1000_000_000).toULong()
         }
+
         ResolutionLevel.Level9 -> {
             // count picoseconds
             hour * 3600_000_000_000_000UL + minute * 60_000_000_000_000UL + (second * 1000_000_000_000).toULong()
@@ -158,19 +160,21 @@ fun encode(input: String): AtDate {
     // read offset part of iso format
 
     val (zoneLevel, zoneULong) = when {
-        timezonePart.endsWith("Z") -> Pair(providedZoneLevel?:ZoneLevel.Level1, 0UL)
+        timezonePart.endsWith("Z") -> Pair(providedZoneLevel ?: ZoneLevel.Level1, 0UL)
         timezonePart.contains("+") -> {
             val offset = timezonePart.substringAfterLast("+").substringBeforeLast("@").split(":")
             val z = offset[0].toByte() * 4 + offset[1].toByte() / 15
-            Pair(providedZoneLevel?:ZoneLevel.Level1, (z and 0b00111111).toULong())
+            Pair(providedZoneLevel ?: ZoneLevel.Level1, (z and 0b00111111).toULong())
         }
+
         timezonePart.contains("-") -> {
             val offset = timezonePart.substringAfterLast("-").substringBeforeLast("@").split(":")
             val z = offset[0].toByte() * 4 + offset[1].toByte() / 15
             val z6bits = z and 0b00111111
-            Pair(providedZoneLevel?:ZoneLevel.Level1, (z6bits or 0b01000000).toULong())
+            Pair(providedZoneLevel ?: ZoneLevel.Level1, (z6bits or 0b01000000).toULong())
         }
-        else -> Pair(providedZoneLevel?:ZoneLevel.Level0, null)
+
+        else -> Pair(providedZoneLevel ?: ZoneLevel.Level0, null)
     }
 
     return AtDate(
@@ -188,8 +192,8 @@ fun encode(input: String): AtDate {
 }
 
 fun getJDN(year: Int, month: Int, day: Int): Int {
-    val a = (14 - month) / 12
-    val y = year + 4800 - a
-    val m = month + 12 * a - 3
-    return day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32046
+    val p1 = (1461 * (year + 4800 + (month - 14) / 12)) / 4
+    val p2 = (367 * (month - 2 - 12 * ((month - 14) / 12))) / 12
+    val p3 = -(3 * ((year + 4900 + (month - 14) / 12) / 100)) / 4 + day - 32075
+    return (p1 + p2 + p3)
 }
--- src/test/kotlin/atdate/encode.test.kt
+++ src/test/kotlin/atdate/encode.test.kt
@@ -13,7 +13,7 @@ class TestEncode {
             resolutionLevel = ResolutionLevel.Level5,
             zoneLevel = ZoneLevel.Level1,
             accuracy = Accuracy.Start,
-            date = 0B11_11110000U,
+            date = 0B11_11110001U,
             time = 0B1_00010111_10011100U,
             zone = 0B1000U,
             plusLeapSeconds = null,
@@ -33,7 +33,7 @@ class TestEncode {
             resolutionLevel = ResolutionLevel.Level5,
             zoneLevel = ZoneLevel.Level1,
             accuracy = Accuracy.Start,
-            date = 0B11_11110000U,
+            date = 0B11_11110001U,
             time = 0B1_00010111_10011100U,
             zone = 0U,
             plusLeapSeconds = null,
@@ -53,12 +53,37 @@ class TestEncode {
             resolutionLevel = ResolutionLevel.Level5,
             zoneLevel = ZoneLevel.Level0,
             accuracy = Accuracy.Start,
-            date = 0B11_11110000U,
+            date = 0B11_11110001U,
             time = 0B1_00010111_10011100U,
             zone = null,
             plusLeapSeconds = null,
             minusLeapSeconds = null,
         )
+        println(actual)
+        println(expected)
+        assert(actual == expected)
+    }
+}
+
+/**
+ * test cases tested from https://aa.usno.navy.mil/data/JulianDate
+ */
+class TestGetJDN {
+    @Test
+    fun test20190505() {
+        val input = "2019-05-05"
+        val (year, month, day) = input.split("-").map { it.toInt() }
+        val actual = getJDN(year, month, day)
+        val expected = 0B00100101_10000011_11110001 // 2458609
+        println(actual)
+        println(expected)
+        assert(actual == expected)
+    }
+
+    @Test
+    fun testDay0() {
+        val actual = getJDN(-4713, 11, 24) // year: 4714BC month: 11 day: 24
+        val expected = 0
         println(actual)
         println(expected)
         assert(actual == expected)
