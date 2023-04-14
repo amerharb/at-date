5bd97c87b4320eee0cc552407d268f2272fb7dd9
Amer Harb <amer.harb@gmail.com>
2023-04-14 07:12:58 +0200

    add function to return number of bits

--- src/main/kotlin/com/amerharb/atdate/AtDate.kt
+++ src/main/kotlin/com/amerharb/atdate/AtDate.kt
@@ -1,6 +1,7 @@
 package com.amerharb.atdate
 
 data class AtDate(
+    // TODO: support period
     val rangeLevel: RangeLevel = RangeLevel.Level1,
     val resolutionLevel: ResolutionLevel = ResolutionLevel.Level0,
     val zoneLevel: ZoneLevel = ZoneLevel.Level0,
@@ -11,7 +12,131 @@ data class AtDate(
     val zone: ULong? = null,
     val plusLeapSeconds: ULong? = null,
     val minusLeapSeconds: ULong? = null,
-)
+) {
+    fun getPayload(): Array<UByte> {
+        val payload = mutableListOf<UByte>()
+        payload.addAll(getHeader())
+        // TODO: add body byes
+        return payload.toTypedArray()
+    }
+
+    private fun getHeader(): Array<UByte> {
+        // TODO: later to support header with more than 1 byte
+        /** header design IKDTTTZA */
+        var header: UByte = 0U
+        header = header or 0B1000_0000U // I
+        header = header or 0B0100_0000U // K
+        header = when (rangeLevel) { // D
+            RangeLevel.Level0 -> TODO()
+            RangeLevel.Level1 -> header and 0B1101_1111U
+            RangeLevel.Level2 -> header or 0B0010_0000U
+            RangeLevel.Level3 -> TODO() // needs 2-bytes header
+            RangeLevel.Level4 -> TODO()
+        }
+        header = when (resolutionLevel) { // TTT
+            ResolutionLevel.Level0 -> header or 0B0000_0000U
+            ResolutionLevel.Level1 -> header or 0B0000_0100U
+            ResolutionLevel.Level2 -> header or 0B0000_1000U
+            ResolutionLevel.Level3 -> header or 0B0000_1100U
+            ResolutionLevel.Level4 -> header or 0B0001_0000U
+            ResolutionLevel.Level5 -> header or 0B0001_0100U
+            ResolutionLevel.Level6 -> header or 0B0001_1000U
+            ResolutionLevel.Level7 -> header or 0B0001_1100U
+            ResolutionLevel.Level8 -> TODO()
+            ResolutionLevel.Level9 -> TODO()
+            ResolutionLevel.Level10 -> TODO()
+            ResolutionLevel.Level11 -> TODO()
+            ResolutionLevel.Level12 -> TODO()
+            ResolutionLevel.Level13 -> TODO()
+            ResolutionLevel.Level14 -> TODO()
+            ResolutionLevel.Level15 -> TODO()
+            ResolutionLevel.Level16 -> TODO()
+            ResolutionLevel.Level17 -> TODO()
+            ResolutionLevel.Level18 -> TODO()
+            ResolutionLevel.Level19 -> TODO()
+            ResolutionLevel.Level20 -> TODO()
+        }
+        header = when (zoneLevel) { // Z
+            ZoneLevel.Level0 -> header or 0B0000_0000U
+            ZoneLevel.Level1 -> header or 0B0000_0010U
+            ZoneLevel.Level2 -> TODO()
+            ZoneLevel.Level3 -> TODO()
+            ZoneLevel.Level4 -> TODO()
+            ZoneLevel.Level5 -> TODO()
+            ZoneLevel.Level6 -> TODO()
+            ZoneLevel.Level7 -> TODO()
+            ZoneLevel.Level8 -> TODO()
+            ZoneLevel.Level9 -> TODO()
+            ZoneLevel.Level10 -> TODO()
+        }
+        header = when (accuracy) { // A
+            Accuracy.Start -> header or 0B0000_0000U
+            Accuracy.Whole -> header or 0B0000_0001U
+            Accuracy.End -> TODO()
+        }
+        return arrayOf(header)
+    }
+
+    private fun getBodyBitCount(): Int {
+        return getRangeBitCount() + getResolutionBitCount() + getZoneBitCount() + getLeapSecondsBitCount()
+    }
+
+    private fun getRangeBitCount(): Int {
+        return when (rangeLevel) {
+            RangeLevel.Level0 -> 0
+            RangeLevel.Level1 -> 15
+            RangeLevel.Level2 -> 22
+            RangeLevel.Level3 -> 32
+            RangeLevel.Level4 -> 48
+        }
+    }
+
+    private fun getResolutionBitCount(): Int {
+        return when (resolutionLevel) {
+            ResolutionLevel.Level0 -> 0
+            ResolutionLevel.Level1 -> 5
+            ResolutionLevel.Level2 -> 7
+            ResolutionLevel.Level3 -> 9
+            ResolutionLevel.Level4 -> 11
+            ResolutionLevel.Level5 -> 17
+            ResolutionLevel.Level6 -> 27
+            ResolutionLevel.Level7 -> 37
+            ResolutionLevel.Level8 -> 47
+            ResolutionLevel.Level9 -> 57
+            ResolutionLevel.Level10 -> 67
+            ResolutionLevel.Level11 -> 77
+            ResolutionLevel.Level12 -> 87
+            ResolutionLevel.Level13 -> 97
+            ResolutionLevel.Level14 -> 107
+            ResolutionLevel.Level15 -> 117
+            ResolutionLevel.Level16 -> 127
+            ResolutionLevel.Level17 -> 136
+            ResolutionLevel.Level18 -> 146
+            ResolutionLevel.Level19 -> 156
+            ResolutionLevel.Level20 -> TODO()
+        }
+    }
+
+    private fun getZoneBitCount(): Int {
+        return when (zoneLevel) {
+            ZoneLevel.Level0 -> 0
+            ZoneLevel.Level1 -> 7
+            ZoneLevel.Level2 -> 11
+            ZoneLevel.Level3 -> 27
+            ZoneLevel.Level4 -> 47
+            ZoneLevel.Level5 -> 67
+            ZoneLevel.Level6 -> 87
+            ZoneLevel.Level7 -> 107
+            ZoneLevel.Level8 -> 127
+            ZoneLevel.Level9 -> 146
+            ZoneLevel.Level10 -> TODO()
+        }
+    }
+
+    private fun getLeapSecondsBitCount(): Int {
+        return leapSecondsFlag.toInt() * 8 * 2
+    }
+}
 
 enum class Accuracy {
     Start,
