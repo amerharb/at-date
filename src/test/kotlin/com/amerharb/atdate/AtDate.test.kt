8b551b65f2f312085dfb6170ff2071bb8dbe0302
Amer Harb <amer.harb@gmail.com>
2023-04-14 12:48:48 +0200

    add example1 to test

--- src/test/kotlin/com/amerharb/atdate/AtDate.test.kt
+++ src/test/kotlin/com/amerharb/atdate/AtDate.test.kt
@@ -3,23 +3,45 @@ package com.amerharb.atdate
 import org.junit.jupiter.api.Test
 
 class TestAtDate {
-    val datetimezone = AtDate(
-        rangeLevel = RangeLevel.Level1,
-        resolutionLevel = ResolutionLevel.Level5,
-        zoneLevel = ZoneLevel.Level1,
-        accuracy = Accuracy.Start,
-        date = 0B11_11110001U,
-        time = 0B1_00010111_10011100U,
-        zone = 0B1000U,
-        plusLeapSeconds = null,
-        minusLeapSeconds = null,
-    )
+    @Test
+    @OptIn(ExperimentalUnsignedTypes::class)
+    fun testGetPayloadExample1() {
+        val example1 = AtDate(
+            rangeLevel = RangeLevel.Level1,
+            resolutionLevel = ResolutionLevel.Level0,
+            zoneLevel = ZoneLevel.Level0,
+            accuracy = Accuracy.Start,
+            date = 0B11_11110001U,
+            time = null,
+            zone = null,
+            plusLeapSeconds = null,
+            minusLeapSeconds = null,
+        )
+        // 0xC007E2
+        val expected = ubyteArrayOf(0xC0U, 0x07U, 0xE2U).toTypedArray()
+        val actual = example1.getPayload()
+        println(expected.joinToString { it.toString(16) })
+        println(actual.joinToString { it.toString(16) })
+        assert(expected.contentEquals(actual))
+    }
 
     @Test
     @OptIn(ExperimentalUnsignedTypes::class)
     fun testGetPayloadExample2() {
+        val example2 = AtDate(
+            rangeLevel = RangeLevel.Level1,
+            resolutionLevel = ResolutionLevel.Level5,
+            zoneLevel = ZoneLevel.Level1,
+            accuracy = Accuracy.Start,
+            date = 0B11_11110001U,
+            time = 0B1_00010111_10011100U,
+            zone = 0B1000U,
+            plusLeapSeconds = null,
+            minusLeapSeconds = null,
+        )
+        // 0xD6 07 E3 17 9C 10
         val expected = ubyteArrayOf(0xD6U, 0x07U, 0xE3U, 0x17U, 0x9CU, 0x10U).toTypedArray()
-        val actual = datetimezone.getPayload()
+        val actual = example2.getPayload()
         println(expected.joinToString { it.toString(16) })
         println(actual.joinToString { it.toString(16) })
         assert(expected.contentEquals(actual))
