7e1b4dfcc31803052ca05a73129cd9dd9f62f733
Amer Harb <amer.harb@gmail.com>
2023-04-15 09:30:03 +0200

    fix showing result

--- src/main/kotlin/com/amerharb/atdate/Main.kt
+++ src/main/kotlin/com/amerharb/atdate/Main.kt
@@ -4,7 +4,9 @@ fun main(args: Array<String>) {
     println("@Date")
     println("input: ${args.joinToString()}")
     if (args.isEmpty()) {
-        mainMenu()
+        while (true) {
+            mainMenu()
+        }
     } else {
         val arg1 = args[0]
         if (arg1 == "-h" || arg1 == "--help") {
@@ -18,15 +20,16 @@ fun main(args: Array<String>) {
             exitProcess(0)
         }
         if (arg1.startsWith("@")) {
-            println("Encoding")
+            println("Encoding...")
             val atDate = encode(arg1)
-            println("Hex: 0x${atDate.getPayload().joinToString("") { it.toString(16) }}")
-            println("Bin: 0b${atDate.getPayload().joinToString("") { it.toString(2) }}")
+            printEncodingResult(atDate.getPayload())
         }
         if (arg1.startsWith("0x")) {
-            println("Decoding")
+            println("Decoding...")
+            val arrayOfBytes = getByteArrayFromHexString(arg1)
+            val atDate = decode(arrayOfBytes)
+            println("Notation: 0x${atDate.getNotation()}")
         }
-        val atDate = encode(args[0])
     }
 }
 
@@ -35,26 +38,56 @@ fun mainMenu() {
     println("2. Decode")
     println("3. Exit")
     print("Enter your choice: ")
-    when (readlnOrNull()?.toInt()) {
-        1 -> encodeMenu()
-        2 -> TODO()
-        3 -> exitProcess(0)
+    when (readlnOrNull()) {
+        "1" -> encodeMenu()
+        "2" -> decodeMenu()
+        "3" -> exitProcess(0)
         else -> {
             println("Invalid choice")
-            mainMenu()
         }
     }
 }
 
 fun encodeMenu() {
-    print("Enter date in @Date format: ")
-    val input = readlnOrNull() ?: return
-    val atDate = encode(input)
-    println(atDate.getPayload().joinToString { it.toString(16) })
-    mainMenu()
+    try {
+        print("Enter date in @Date format: ")
+        val input = readlnOrNull() ?: return
+        val atDate = encode(input)
+        printEncodingResult(atDate.getPayload())
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
+    }
+}
+
+fun decodeMenu() {
+    try {
+        print("Enter payload in Hex format (0x1234..) :")
+        val input = readlnOrNull() ?: return
+        val arrayOfUBytes =  getByteArrayFromHexString(input)
+        val atDate = decode(arrayOfUBytes)
+        println("Notation: ${atDate.getNotation()}")
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
+    }
 }
 
 fun exitProcess(status: Int) {
     println("Exiting with status $status")
     kotlin.system.exitProcess(status)
+}
+
+fun printEncodingResult(payload: Array<UByte>) {
+    println("Hex: 0x${payload.joinToString("") { it.toString(16).padStart(2, '0') }}")
+    println("Bin: 0b${payload.joinToString("") { it.toString(2).padStart(8, '0') }}")
+}
+
+fun getByteArrayFromHexString(hexString: String): Array<UByte> {
+    require(hexString.startsWith("0x")) { "Hex string must start with '0x'." }
+    require(hexString.length % 2 == 0) { "Hex string length must be even." }
+
+    return hexString
+        .substring(2)
+        .chunked(2)
+        .map { it.toUByte(16) }
+        .toTypedArray()
 }
