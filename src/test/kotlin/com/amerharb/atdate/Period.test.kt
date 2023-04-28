a1d114d825452bc06f078ff4fea2f58f56264106
Amer Harb <amer.harb@gmail.com>
2023-04-28 11:36:38 +0200

    add Period.test.kt

---
+++ src/test/kotlin/com/amerharb/atdate/Period.test.kt
@@ -0,0 +1,37 @@
+package com.amerharb.atdate
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+class TestPeriod {
+    val example4 = Period(
+        sign = true,
+        rangeLevel = RangeLevel.Level1,
+        resolutionLevel = ResolutionLevel.Level0,
+        date = 1U,
+        time = null,
+        plusLeapSeconds = null,
+        minusLeapSeconds = null,
+    )
+
+    @Test
+    @OptIn(ExperimentalUnsignedTypes::class)
+    fun testGetPayloadExample4() {
+        // 0x880002
+        val expected = ubyteArrayOf(0x88U, 0x00U, 0x02U).toTypedArray()
+        val actual = example4.getPayload()
+        println(expected.joinToString { it.toString(16) })
+        println(actual.joinToString { it.toString(16) })
+        assertContentEquals(expected, actual)
+    }
+
+    @Test
+    fun testGetNotationExample4() {
+        val expected = "@P+1D { d:1 t:0 l:0-0 }@"
+        val actual = example4.getNotation()
+        println(expected)
+        println(actual)
+        assertEquals(expected, actual)
+    }
+}
