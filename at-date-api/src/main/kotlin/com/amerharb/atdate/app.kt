9a7fe407dbf91c807bbeb3939896ee4ef3ca745b
Amer Harb <amer.harb@gmail.com>
2023-06-14 14:28:22 +0200

    WiP add 2 endpoint for encode and decode

--- at-date-api/src/main/kotlin/com/amerharb/atdate/app.kt
+++ at-date-api/src/main/kotlin/com/amerharb/atdate/app.kt
@@ -1,3 +1,5 @@
+import com.amerharb.atdate.decode
+import com.amerharb.atdate.encode
 import io.ktor.server.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -15,5 +17,42 @@ fun Application.module() {
         get("/") {
             call.respondText("Hello, world!")
         }
+
+        post("/encode/{notation}") {
+            val notation = call.parameters["notation"]
+            if (notation == null) {
+                call.respondText("Notation is null")
+                return@post
+            }
+            val ad = encode(notation)
+            if (ad == null) {
+                call.respondText("Notation is invalid")
+            } else {
+                //TODO: return json
+                call.respondText(ad.toString())
+            }
+        }
+
+        post("/decode/{hex}") {
+            val hex = call.parameters["hex"]
+            if (hex == null) {
+                call.respondText("Hex is null")
+                return@post
+            }
+            // convert hex text into Array[UByte]
+            val bArray = getUByteArray(hex)
+            val ad = decode(bArray)
+            //TODO: return json
+            call.respondText(ad.toString())
+        }
     }
+}
+
+fun getUByteArray(hex: String): Array<UByte> {
+    // convert hex start with 0x into Array[UByte]
+    val byteList = hex
+        .drop(2)
+        .chunked(2)
+        .map { it.toInt(16).toUByte() }
+    return byteList.toTypedArray()
 }
