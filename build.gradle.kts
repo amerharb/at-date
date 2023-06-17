c3dc5ade0d34af14c758dcc090c542d9fb84b58a
Amer Harb <amer.harb@gmail.com>
2023-06-17 18:06:30 +0200

    add lint plugins to root modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,35 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+	kotlin("jvm") version "1.8.0"
+	id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
 }
 
 group = "com.amerharb.atdate"
 version = "0.1.0"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
+}
+
+ktlint {
+	verbose.set(true)
+	outputToConsole.set(true)
+	coloredOutput.set(true)
+}
+ktlint {
+	version.set("0.49.1")
+	verbose.set(true)
+	outputToConsole.set(true)
+	coloredOutput.set(true)
+}
+
+subprojects {
+	apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+	repositories {
+		mavenCentral()
+	}
+
+	configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+		debug.set(true)
+	}
 }
