b91d0a3a0df169cda3791ea42e02eafad9d709ad
Amer Harb <amer.harb@gmail.com>
2023-06-22 10:22:47 +0200

    fix wildcard import

--- at-date-api/src/main/kotlin/com/amerharb/atdate/App.kt
+++ at-date-api/src/main/kotlin/com/amerharb/atdate/App.kt
@@ -1,13 +1,16 @@
 package com.amerharb.atdate
 
-import io.ktor.http.*
-import io.ktor.server.application.*
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import java.util.*
-
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import java.util.Base64
 const val PORT = 8000
 
 fun main() {
