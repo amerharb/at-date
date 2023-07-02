7555b7dca556298d3eca040f23b37a62d2657ca5
Amer Harb <amer.harb@gmail.com>
2023-07-02 12:37:04 +0200

    add web module

---
+++ at-date-web-client/src/main/kotlin/com/amerharb/atdate/Application.kt
@@ -0,0 +1,15 @@
+package com.amerharb.atdate
+
+import com.amerharb.atdate.plugins.configureRouting
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+	embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+		.start(wait = true)
+}
+
+fun Application.module() {
+	configureRouting()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "at-date"
 include("at-date-lib")
 include("at-date-console")
 include("at-date-api")
+include("at-date-web-client")
