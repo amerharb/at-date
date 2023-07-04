0814393731a05be212d8af83226d78a022a67de5
Amer Harb <amer.harb@gmail.com>
2023-07-04 10:02:34 +0200

    move variable to the root

--- at-date-api/build.gradle.kts
+++ at-date-api/build.gradle.kts
@@ -2,13 +2,15 @@ plugins {
 	kotlin("jvm") version "1.8.0"
 }
 
+val ktor_version:String by project
+val logback_version:String by project
+
 group = "com.amerharb.atdate"
 version = "0.1.1"
 
-val ktor_version = "2.3.1"
 dependencies {
 	implementation(project(":at-date-lib"))
 	implementation("io.ktor:ktor-server-netty:$ktor_version")
 	implementation("io.ktor:ktor-server-core:$ktor_version")
-	implementation("ch.qos.logback:logback-classic:1.2.9")
+	implementation("ch.qos.logback:logback-classic:$logback_version")
 }
